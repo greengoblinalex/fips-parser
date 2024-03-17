dc4eaf6ed2dc669cd03ef3009be63b21f954294e
greengoblinalex <greengoblinalex@mail.ru>
2024-03-17 21:57:42 +0300

    update

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     with FipsParser() as fips_parser:
         url_text_map = fips_parser.parse_by_query('Искусственный интеллект в нефтегазе')
 
-    algorithms = [
+    phrases = [
         'искусственный интеллект',
         'Original Equipment Manufacture',
         'диагностика LMDS',
@@ -16,7 +16,7 @@ def main():
 
     for url, text in url_text_map.items():
         print(f'Ссылка на патент: {url}\nНайденные алгоритмы:')
-        phrases_found = text_parser.search_phrases_in_text(algorithms, text)
+        phrases_found = text_parser.search_phrases_in_text(phrases, text)
         if phrases_found:
             pprint(phrases_found)
         else:
